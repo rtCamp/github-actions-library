9bc21a7722207ba50a3fc1e2b4ebeb62a8a06435
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-01-29 20:22:26 +0530

    Convert to lowercase to handle all types of true input
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -48,7 +48,7 @@ rm -rf uploads
 ln -s ../../../uploads uploads
 
 # Setup mu-plugins if VIP
-VIP=$(cat "$hosts_file" | shyaml get-value "$CI_SCRIPT_OPTIONS.vip")
+VIP=$(cat "$hosts_file" | shyaml get-value "$CI_SCRIPT_OPTIONS.vip" | tr '[:upper:]' '[:lower:]')
 if [ "$VIP" = "true" ]; then
     MU_PLUGINS_URL=${MU_PLUGINS_URL:-"https://github.com/Automattic/vip-mu-plugins-public"}
     MU_PLUGINS_DIR="$HTDOCS/wp-content/mu-plugins"
