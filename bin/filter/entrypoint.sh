0e56bb579e7f15a407811dc3a3556b0c5bb68876
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-02-26 18:22:13 +0530

    Update entrypoint and permisson setting
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- bin/filter/entrypoint.sh
+++ bin/filter/entrypoint.sh
@@ -1,5 +1,12 @@
-#!/bin/sh 
+#!/usr/bin/env bash
 
-set -e
+# custom path for files to override default files
+custom_path="$GITHUB_WORKSPACE/.github/bin"
+main_script="/usr/local/bin/branch"
 
-sh -c "$*"
+if [[ -d "$custom_path" ]]; then
+    rsync -av "$custom_path/" /usr/local/bin/
+    RUN chmod +x -R /usr/local/bin/
+fi
+
+bash "$main_script"
